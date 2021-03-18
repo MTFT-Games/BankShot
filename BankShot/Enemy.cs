61c66f02882c60919d43253e6319a77fc13b66a6
Machi Dimps <machidimps@gmail.com>
2021-03-18 14:36:19 -0400

    Enemy constructor and Pathfinder beginnings

--- BankShot/Enemy.cs
+++ BankShot/Enemy.cs
@@ -13,13 +13,25 @@ namespace BankShot
 
         //Enemy Stats will be included as Fields
 
+        //Constructor
+        public Enemy(Texture2D texture, Rectangle transform, List<Rectangle> collisionBoxes, bool active, int maxHealth, Vector2 velocity)
+            : base(texture,transform,collisionBoxes,active,maxHealth,velocity)
+        {
+
+        }
         //Methods
         public void DealDamage(IDamagable target)) { }
 
         //I'm not sure that we know what these methods do yet
         //so I am just including the list we had in the 
         //diagram.
+        public bool Pathfind(GameObject target)
+        {
+            bool shouldTurn = false;
 
+
+            return shouldTurn;
+        }
         //Pathfind, Move at player, Target player, General ai functions, Attack player
     }
 }
