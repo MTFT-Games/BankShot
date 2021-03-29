20deb99051d68228bd8d2ed3dc6b970ec3e9e878
Aidan VanBurger <ajv4288@g.rit.edu>
2021-03-29 16:12:33 -0400

    added fsms to update and draw

--- BankShot/Game1.cs
+++ BankShot/Game1.cs
@@ -17,11 +17,13 @@ namespace BankShot {
     public class Game1 : Game {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        //font and menu obj for testing- to be replaced
+        //font, texture and menu obj for testing- to be replaced
         private SpriteFont font;
         private MainMenu mnu;
+        private Texture2D buttonTx;
 
         public static EnemyManager enemyManager;
+        public GameState state;
 
         //Testing player
         public Player player;
@@ -39,16 +41,21 @@ namespace BankShot {
 
         protected override void Initialize() {
             // TODO: Add your initialization logic here
-            //menu to be replaced
-            mnu = new MainMenu(font);
+            state = GameState.MainMenu;
+           
             
             base.Initialize();
         }
 
         protected override void LoadContent() {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            //font for testing- to be replaced
+
+            //menu testing
             font = Content.Load<SpriteFont>("File");
+            buttonTx = Content.Load<Texture2D>("button1");
+            mnu = new MainMenu(font, buttonTx);
+            //end menu testing
+
 
             //Testing gun and projectile creation.
             /*
@@ -63,6 +70,31 @@ namespace BankShot {
         protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            //gathers keybaord and mouse states for use in update methods
+            KeyboardState kbs = Keyboard.GetState();
+            MouseState ms = Mouse.GetState();
+
+            //state machine based on the GameState enum
+            switch (state)
+            {
+                case GameState.MainMenu:
+
+                    break;
+                case GameState.Game:
+
+                    break;
+                case GameState.Pause:
+
+                    break;
+                case GameState.Leaderboard:
+
+                    break;
+                case GameState.GameOver:
+
+                    break;
+
+            }
+
 
 
             // TODO: Add your update logic here
@@ -76,13 +108,39 @@ namespace BankShot {
 
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            //begins spritebatch
             _spriteBatch.Begin();
-            //mnu.Draw(_spriteBatch, _graphics);
+            
+        
+            //state machine based on the GameState enum
+            switch (state)
+            {
+                case GameState.MainMenu:
+
+                    //for menu testing
+                    _spriteBatch.DrawString(font, state.ToString(), new Vector2(10, 10), Color.White);
+                    mnu.Draw(_spriteBatch, _graphics);
+                    //end menu testing
+
+                    break;
+                case GameState.Game:
+
+                    break;
+                case GameState.Pause:
+
+                    break;
+                case GameState.Leaderboard:
+
+                    break;
+                case GameState.GameOver:
+
+                    break;
+
+            }
 
             //Testing gun and projectile creation.
             //gun.Draw(_spriteBatch);
-            
+
             _spriteBatch.End();
             base.Draw(gameTime);
         }
