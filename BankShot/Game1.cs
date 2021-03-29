391de568d03357feed2643a5c722ff3b775c1703
Aidan VanBurger <ajv4288@g.rit.edu>
2021-03-29 15:45:55 -0400

    completed menus

--- BankShot/Game1.cs
+++ BankShot/Game1.cs
@@ -47,6 +47,7 @@ namespace BankShot {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
