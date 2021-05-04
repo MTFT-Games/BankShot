b8873942a75aae4337da647136103f6ec957f8c8
Michael Robinson <mhr2964@rit.edu>
2021-05-04 19:40:48 -0400

    Not broken finally

--- ExternalTool/Editor.cs
+++ ExternalTool/Editor.cs
@@ -411,6 +411,7 @@ namespace ExternalTool
                         } else
                         {
                             writer.Write(".|");
+                            //
                         }
                     }
                         
